9d8d9a2548e83579b3fb9455c24932e0b2fd51ea
gianna.jolibeth15 <gianna.jolibeth15@gmail.com>
2018-09-28 23:03:42 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,14 @@
 def hello_t(array)
-
+i=0 
+while i< array.length 
+yield array[i]
+i+=1 
+end 
 end
 
 # call your method here!
-
+hello_t(["Tim","Tom", "Jim"]) do |name|
+  if name.stat_with?("T")
+    puts "Hi, #{name}"
+  end
+end
